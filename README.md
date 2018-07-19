993031aa20ebc6aa2639ef6b11a61cfc826af203
mercury <udtrokia@gmail.com>
2018-07-19 14:12:07 +0800

    [complete]: lesson 1

---
+++ README.md
@@ -0,0 +1,48 @@
+## Lesson 1
+
+<br><br>
+### Layers
+
++ Cocoa Touch
+  + Multi-Touch - Alerts
+  + Core Motion - Web View
+  + View Hierarchy - Map kit
+  + Localization - Image Picker
+  + Controls - Camera
++ Media
+  + Core Audio - JPEG, PNG, TIFF
+  + OpenAL - PDF
+  + Audio Mixing - Quartz (2D)
+  + Audio Recording - Core Animation
+  + Video Playback - OpenGL ES
++ Core Services
+  + Collections - Core Location
+  + Address Book - Net Services
+  + Networking - Threading
+  + File Access - Preferences
+  + SQLite - URL Utilities
++ Core OS
+  + Core OS
+  + OSX Kernel - Power Management
+  + Mach 3.0 - Keychain Access
+  + BSD - Certificates
+  + Sockets - File System
+  + Security - Bonjour
+
+<br><br>
+### Platform Components
+
++ Tools 
+  + Xcode 
+  + Instrumetns
++ Language(s)
+  + Object-C 
+  + Swift
++ Frameworks
+  + Foundation
+  + Core Data
+  + UIKit
+  + Map Kit
+  + Core Motion
++ Design Strategy
+  + MVC
--- blade/ViewController.swift
+++ blade/ViewController.swift
@@ -15,11 +15,22 @@ class ViewController: UIViewController {
 // ViewController => link UI layer name, if you want change, change both of them
 // UIViewController father class
     
+    var flipCount = 0 {
+        didSet {
+            flipCountLabel.text = "Flips: \(flipCount)";
+        }
+    }
     
+    var emojiChoices: Array<String> = ["🎃", "👻", "🎃", "👻"];
+    
+    @IBOutlet weak var flipCountLabel: UILabel!
+    @IBOutlet var cardButtons: [UIButton]!
+    // action create method, outlet create property.
     @IBAction func touchCard(_ sender: UIButton) {
     // @IBAction is not part of a Swift method, a special directive that Xcode is putting in there.
-        print("agh! a ghost!");
-        flipCard(withEmoji: "👻", on: sender)
+        flipCount += 1;
+        let cardNumber = cardButtons.index(of: sender)!
+        flipCard(withEmoji: emojiChoices[cardNumber], on: sender);
     }
     
     func flipCard(withEmoji emoji: String, on button: UIButton) {
@@ -28,6 +39,7 @@ class ViewController: UIViewController {
             button.setTitle("", for: UIControlState.normal)
             button.backgroundColor = #colorLiteral(red: 1, green: 0.5763723254, blue: 0, alpha: 1)
         } else {
+            if flipCount == 2 { print("congratulation!") }
             button.setTitle(emoji, for: UIControlState.normal)
             button.backgroundColor = #colorLiteral(red: 1.0, green: 1.0, blue: 1.0, alpha: 1.0)
         }
